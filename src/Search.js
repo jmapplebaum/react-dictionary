6a3cb776e8cdc09a528407b8bf0a5c194b289d63
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-06-17 17:26:17 -0500

    Added axios and integrated SheCodes api

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function handleSearch(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}...`);
+
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=5b4802f40a5b2aoe7a3t7b824a662fdf`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function keywordChange(event) {
     setKeyword(event.target.value);
   }
