7d14c2b08459a5abe0b31e44206236cdc310b0af
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-06-17 20:22:40 -0500

    Removed the alert when handleSearch() is called

--- src/Search.js
+++ src/Search.js
@@ -14,7 +14,6 @@ export default function Search() {
 
   function handleSearch(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}...`);
 
     const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=5b4802f40a5b2aoe7a3t7b824a662fdf`;
     axios.get(apiUrl).then(handleResponse);
