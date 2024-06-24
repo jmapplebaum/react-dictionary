b37b5858abaf1b4ed8569301960c744c74e46a0c
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-06-24 18:37:23 -0500

    Created Synonym.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,18 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   console.log({ props });
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
       <p>
-        Definition: {props.meaning.definition}
+        <strong>Definition: </strong>
+        {props.meaning.definition}
         <br />
-        <em>Example: {props.meaning.example}</em>
+        <strong>Example: </strong>
+        <em>{props.meaning.example}</em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
   );
