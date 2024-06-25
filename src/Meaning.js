373dfaf8350816ad677ccab22ab7b0199673d73a
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-06-24 20:28:49 -0500

    Added Phonetics.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,22 +3,20 @@ import Example from "./Example";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  const definition = props.meaning.definitions[0]; // Only the first definition
+
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              <strong>Definition:</strong> {definition.definition}
-              <br />
-              <Example example={definition.example} />
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
-          </div>
-        );
-      })}
+      <div>
+        <p>
+          <strong>Definition:</strong> {definition.definition}
+          <br />
+          <Example example={definition.example} />
+          <br />
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </p>
+      </div>
     </div>
   );
 }
