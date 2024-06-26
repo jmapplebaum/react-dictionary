953b19f10aec2fb31ce64d04a32b2cf13121cc74
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-06-25 21:38:06 -0500

    Added styling and set default keyword to "dictionary"

--- src/Search.js
+++ src/Search.js
@@ -1,34 +1,52 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
-import "./Search.css";
 
 export default function Search() {
-  let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("dictionary");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     console.log(response.data);
     setResults(response.data[0]);
   }
+  function search() {
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   function handleSearch(event) {
     event.preventDefault();
-
-    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
   }
 
   function keywordChange(event) {
     setKeyword(event.target.value);
   }
+  function load() {
+    setLoaded(true);
+    search();
+  }
 
-  return (
-    <div className="Search">
-      <form onSubmit={handleSearch} className="text-center">
-        <input type="search" onChange={keywordChange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Search">
+        <section>
+          <h4>What word do you want to look up?</h4>
+          <form onSubmit={handleSearch} className="text-center">
+            <input
+              type="search"
+              placeholder="🔍 search for a word"
+              onChange={keywordChange}
+            />
+          </form>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
